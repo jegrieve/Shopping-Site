67346ae7a5fb7bae454a49494e5350c6f41c4cfb
James Grieve <pocojames@hotmail.com>
2021-04-16 15:30:24 -0700

    add each component to corresponding routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import HomePage from './components/Homepage';
+import Navbar from './components/Navbar';
+import ShopPage from './components/ShopPage';
+import CartPage from './components/CartPage';
+
 
 const Routes = () => {
   return (
     <BrowserRouter>
+        <Navbar />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/cart" component={CartPage} />
       </Switch>
     </BrowserRouter>
   );
